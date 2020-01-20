f4a75cc14b1e4835b2f1ac0cca59dc78aed818ce
cell <549024307@qq.com>
2020-01-20 14:27:20 +0800

    no message

--- src/common/githubOperate.js
+++ src/common/githubOperate.js
@@ -2,10 +2,6 @@ import common from './common.js'
 import axios from './axios.js'
 /* eslint-disable */
 var operate = {
-  // 增
-  addToGitHub() {
-
-  },
   // 删
   deleteToGitHub() {
 
@@ -13,7 +9,6 @@ var operate = {
   // 查
   getFromGitHub(userName, accessToken, path) {
     let url = `https://api.github.com/repos/${userName}/${path.substring(0, path.indexOf('/'))}/contents/${path.substring(path.indexOf('/') + 1)}?access_token=${accessToken}`
-
     axios('get', url).then(res => {
       let data=JSON.parse(res)
       let result=common.Base64.decode(data.content)
@@ -22,11 +17,12 @@ var operate = {
       console.log(fail);
     })
   },
-  // 改
-  UpdateToGitHub() {
+  // 增，改
+  UpdateToGitHub(userName, accessToken, path,msg) {
     let url = `https://api.github.com/repos/${userName}/${path.substring(0, path.indexOf('/'))}/contents/${path.substring(path.indexOf('/') + 1)}?access_token=${accessToken}`
     let params = {}
     operate.getFromGitHub(userName, accessToken, path).then((data) => {
+      // 需要获取文件sha
       params.sha = JSON.parse(data).sha
       params.message = 'upload note'
       // content内容需转成base64
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -21,50 +21,8 @@ export default {
     this.submit()
   },
   methods: {
-    // get_github_content (userName, accessToken, path) {
-    //   let url = `https://api.github.com/repos/${userName}/${path.substring(0, path.indexOf('/'))}/contents/${path.substring(path.indexOf('/') + 1)}?access_token=${accessToken}`
-    //   return new Promise((resolve, reject) => {
-    //     try {
-    //       let xhr = new XMLHttpRequest()
-    //       xhr.onreadystatechange = function () {
-    //         if (xhr.status === 200 && xhr.readyState === 4) {
-    //           resolve(xhr.responseText)
-    //         }
-    //       }
-    //       xhr.open('get', url, true)
-    //       xhr.send()
-    //     } catch (e) {
-    //       reject(e)
-    //     }
-    //   })
-    // },
-    // update_github_marks (userName, accessToken, path, msg) {
-    //   let url = `https://api.github.com/repos/${userName}/${path.substring(0, path.indexOf('/'))}/contents/${path.substring(path.indexOf('/') + 1)}?access_token=${accessToken}`
-    //   let params = {}
-    //   this.get_github_content(userName, accessToken, path).then((data) => {
-    //     params.sha = JSON.parse(data).sha
-    //     params.message = 'upload note'
-    //     // content内容需转成base64
-    //     params.content = common.Base64.encode(JSON.stringify(msg))
-    //     return new Promise((resolve, reject) => {
-    //       try {
-    //         let xhr = new XMLHttpRequest()
-    //         xhr.onreadystatechange = function () {
-    //           if (xhr.status === 200 && xhr.readyState === 4) {
-    //             resolve(xhr.responseText)
-    //           }
-    //         }
-    //         xhr.open('put', url, true)
-    //         xhr.send(JSON.stringify(params))
-    //       } catch (e) {
-    //         reject(e)
-    //       }
-    //     })
-    //   })
-    // },
     submit () {
-      // update_github_marks('zaneblbl', '9e869d2d1736e843d42d254dc690e5c26a753f8b','BookMark/noteBook.json', document.getElementById('note__textarea').value);
-      let result = githubOperate.getFromGitHub('zaneblbl', '9e869d2d1736e843d42d254dc690e5c26a753f8b', 'story/noteBook.json')
+      let result = githubOperate.UpdateToGitHub('zaneblbl', 'b03b144fbd1f10cb1ac033d994358c9190f67728', 'story/test/test.json', 'erqer')
       console.log(result)
     }
 
